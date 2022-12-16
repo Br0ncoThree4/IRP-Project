2c1d488e05844d9b38b9bbd03b95b59726330321
Br0ncoThree4 <120695612+Br0ncoThree4@users.noreply.github.com>
2022-12-15 20:17:35 -0600

    Create ChessBoard.cpp

---
+++ ChessBoard.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include "Chess.cpp"
+using namespace std;
+
+class Board {
+    public:
+        Chess
+        Board(string, int, int);
+    private:
+
+    
+
+
+    Board::Board(string, int r, int f) {//Creates the 2D array that is the board
+        Chess theBoard[8][8];
+
+    }
+}
